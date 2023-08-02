4a6729f6cddb9504b909737a0720a847a191743e
Carl Brugger <carl.brugger@flatfile.io>
2023-08-01 18:28:52 -0600

    Update to test extractors
    Test files included

--- testing/index.ts
+++ testing/index.ts
@@ -1,18 +1,18 @@
-import { UploadCompletedEvent } from "@flatfile/api/api";
-import { FlatfileListener } from "@flatfile/listener";
-import {
-  JSONExtractor,
-  jsonExtractorPlugin,
-} from "@flatfile/plugin-json-extractor";
+import type { FlatfileListener } from '@flatfile/listener'
+import { DelimiterExtractor } from '@flatfile/plugin-delimiter-extractor'
+import { JSONExtractor } from '@flatfile/plugin-json-extractor'
+import { PSVExtractor } from '@flatfile/plugin-psv-extractor'
+import { TSVExtractor } from '@flatfile/plugin-tsv-extractor'
+import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor'
+import { XMLExtractor } from '@flatfile/plugin-xml-extractor'
+import { ZipExtractor } from '@flatfile/plugin-zip-extractor'
 
-export default function (listener: FlatfileListener) {
-  listener.on("**", ({ topic }) => {
-    console.log(`Received event: ${topic}`);
-  });
-  listener.on("file:created", (event) => {
-    return new JSONExtractor(
-      event as unknown as UploadCompletedEvent
-    ).runExtraction();
-  });
-  // jsonExtractorPlugin()(listener);
+export default async function (listener: FlatfileListener) {
+  listener.use(JSONExtractor())
+  listener.use(ExcelExtractor())
+  listener.use(XMLExtractor())
+  listener.use(PSVExtractor())
+  listener.use(TSVExtractor())
+  listener.use(DelimiterExtractor('txt', { delimiter: '~' }))
+  listener.use(ZipExtractor())
 }

c12c45174a0d490945f85a78f377058e986f0064
Jevon Wild <dorkusprime@users.noreply.github.com>
2025-07-29 08:46:38 -0700

    whitespace

--- 102.02-adding-structure/javascript/listeners/contacts-hook.listener.js
+++ 102.02-adding-structure/javascript/listeners/contacts-hook.listener.js
@@ -1,19 +1,25 @@
 import { bulkRecordHook } from "@flatfile/plugin-record-hook";
 
 export const contactsHook = bulkRecordHook('contacts', (records) => {
+
   records.map(record => {
+    
     const emailValue = record.get('email');
+
     if (emailValue) {
+
       const email = emailValue.toLowerCase();
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
       if (!emailRegex.test(email)) {
         record.addError('email', 'Please enter a valid email address (e.g., user@example.com)');
-      }
-      else {
+      } else {
+
         // Normalize the email to lowercase
         record.set('email', email);
       }
     }
+
     return record;
   });
 });
